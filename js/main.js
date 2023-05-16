fe1457e50ec0beb2afe3a7ad4dc294c9f483dd63
Artur <a.wolniak01@gmail.com>
2023-05-17 00:19:58 +0200

    challenge completed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 const questions = document.querySelectorAll('.question')
+const box = document.querySelector('.iconDesktop-box')
+const container = document.querySelector('.container')
+
+const boxAnimOn = () => {
+	box.classList.add('pressed')
+	container.classList.add('pressed')
+}
+
+const boxAnimOff = () => {
+	box.classList.remove('pressed')
+	container.classList.remove('pressed')
+}
 
 questions.forEach(question => {
 	question.addEventListener('click', () => {
 		let activeAnswer = document.querySelector('.active')
+		question.classList.toggle('active')
+
 		if (activeAnswer) {
 			activeAnswer.classList.remove('active')
 		}
-		console.log(activeAnswer)
-		question.classList.toggle('active')
 	})
+})
+
+questions.forEach(question => {
+	question.addEventListener('mousedown', boxAnimOn)
+	question.addEventListener('mouseup', boxAnimOff)
 })
